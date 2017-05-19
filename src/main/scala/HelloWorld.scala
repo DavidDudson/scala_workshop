71c68ad92d5cfd610c7aa5abaf9b5aecaf8d97b3
David Dudson <davidjohndudson@gmail.com>
2017-05-19 21:12:35 +1200

    Add Hello world examples

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,1 +1,28 @@
 
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
+
+object HelloWorldApp extends App {
+  println("Hello, world!")
+}
+
+object HelloWordStringExamples extends App {
+  val hello: String = "Hello"
+
+  println(hello + ", world")
+
+  println(s"$hello, world")
+
+  println("""
+      | Hello,
+      | world!""".stripMargin)
+
+  println(s"""
+      | $hello,
+      | world!""".stripMargin)
+
+  println(raw"""This entire "string" is escaped \n!""")
+}
