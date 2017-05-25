c672d8ff75b1ac9be8c9899205b75ad9f9f6e7e2
David Dudson <d.dudson@psenterprise.com>
2017-05-25 14:31:55 +1200

    Complete the example

--- src/main/scala/JsonParser.scala
+++ src/main/scala/JsonParser.scala
@@ -9,19 +9,20 @@ import cats.implicits._
 
 import scala.io.{BufferedSource, Source}
 
+import java.io.File
+import java.io.PrintWriter
+
 object JsonParser {
 
-  def toNameData(data: Vector[Json]): Option[NameData] = {
-    val strOption = root.string.getOption _
-    (strOption(data(8)), strOption(data(9)), strOption(data(10)), strOption(data(11)), strOption(data(12)))
-      .map5((year, gender, name, ethnicity, count) =>
-        NameData(name, year, ethnicity, count, gender == "MALE"))
+  def writeToFile(p: String, s: String): Unit = {
+    val pw = new PrintWriter(new File(p))
+    try pw.write(s) finally pw.close()
   }
 
   def main(args: Array[String]): Unit = {
     val source: BufferedSource = Source.fromFile("new_york_names.json")
-    val lines: String = try source.mkString finally source.close()
-    val jsonDocument: Either[ParsingFailure, Json] = parse(lines)
+    val text: String = try source.mkString finally source.close()
+    val jsonDocument: Either[ParsingFailure, Json] = parse(text)
 
     if (jsonDocument.isLeft) {
       println(jsonDocument.left.get)
@@ -29,11 +30,22 @@ object JsonParser {
     }
 
     // Prints first person
-    jsonDocument.toOption
-      .flatMap(d => root.data.index(0).arr.getOption(d))
-      .map(toNameData)
-      .foreach(println)
+    val allPeople = jsonDocument.toOption
+      .flatMap(d => root.data.arr.getOption(d))
+      .map(a => a.flatMap(root.arr.getOption(_).toVector))
+      .map(a => a.flatMap(NameData(_).toVector))
+
+    writeToFile("new_york_modified.json", allPeople.asJson.toString)
   }
 }
 
 case class NameData(name: String, birthYear: String, ethnicity: String, count: String, isMale: Boolean)
+
+object NameData {
+  def apply(data: Vector[Json]): Option[NameData] = {
+    val strOption = root.string.getOption _
+    (strOption(data(8)), strOption(data(9)), strOption(data(10)), strOption(data(11)), strOption(data(12)))
+        .map5((year, gender, name, ethnicity, count) =>
+          NameData(name, year, ethnicity, count, gender == "MALE"))
+  }
+}
